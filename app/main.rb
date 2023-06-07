ec539e2d6deb307f317715cbe8121960fe7491c1
vinnydiehl <vinny.diehl@gmail.com>
2023-06-07 08:19:33 -0400

    Get pipes moving across the screen

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,38 @@
+FPS = 60
+
+class PipeGroup
+  SPEED = 2
+  WIDTH = 80
+  GAP = 150
+
+  DELAY = 3 # seconds
+
+  attr_reader :primitives
+
+  def initialize(args)
+    @args = args
+
+    @screen_width = args.grid.w
+    @screen_height = args.grid.h
+
+    @primitives = [(@screen_height + GAP) / 2, -(@screen_height + GAP) / 2].map do |y|
+      {
+        x: @screen_width, y: y,
+        w: WIDTH,
+        h: @screen_height,
+        r: 34, g: 140, b: 34,
+        primitive_marker: :solid
+      }
+    end
+  end
+
+  def advance
+    @primitives.each { |p| p.x -= SPEED }
+  end
+
+  def colliding_with?(rect); end
+end
+
 class FlappyBirdGame
   PLAYER_SIZE = 40
 
@@ -16,15 +51,33 @@ class FlappyBirdGame
       path: "sprites/circle/orange.png",
       primitive_marker: :sprite
     }
+
+    @pipe_groups = []
+    spawn_pipes
   end
 
   def tick
+    handle_pipes
     render
+  end
+
+  def handle_pipes
+    spawn_pipes if @pipe_spawn_timer == 0
+    @pipe_spawn_timer -= 1
+    @pipe_groups.each(&:advance)
+  end
+
+  def spawn_pipes
+    @pipe_groups << PipeGroup.new(@args)
+    @pipe_spawn_timer = PipeGroup::DELAY * FPS
   end
 
   def render
     @primitives << { x: 0, y: 0, w: @screen_width, h: @screen_height,
-                     r: 135, g: 206, b: 235 }.to_solid
+                     r: 135, g: 206, b: 235, primitive_marker: :solid }
+
+    @pipe_groups.each { |p| @primitives << p.primitives }
+
     @primitives << @player
   end
 end
